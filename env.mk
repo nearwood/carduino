fa150a047c452e855d9c298d017b587f3544cdfd
Nick <nearwood@gmail.com>
2014-11-21 16:41:18 -0500

    getting further with build. condensed env.mk stuff.

--- env.mk
+++ env.mk
@@ -2,33 +2,35 @@ SDK=../arduino-1.5.3-Intel.1.0.4
 #GPATH=$(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux/usr/bin
 #SYSROOT=$(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux/usr/bin/i586-poky-linux-uclibc
 
-OECORE_NATIVE_SYSROOT=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/x86_64-pokysdk-linux
-OECORE_TARGET_SYSROOT=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/i586-poky-linux-uclibc
-OECORE_ACLOCAL_OPTS=-I /home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/x86_64-pokysdk-linux/usr/share/aclocal
+OECORE_NATIVE_SYSROOT=$(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux
+OECORE_TARGET_SYSROOT=$(SDK)/hardware/tools/sysroots/i586-poky-linux-uclibc
+OECORE_ACLOCAL_OPTS=-I $(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux/usr/share/aclocal
 OECORE_DISTRO_VERSION=1.4.2
 OECORE_SDK_VERSION=1.4.2
-PYTHONHOME=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/x86_64-pokysdk-linux/usr
+PYTHONHOME=$(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux/usr
 ARCH=x86
-PKG_CONFIG_SYSROOT_DIR=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/i586-poky-linux-uclibc
-PKG_CONFIG_PATH=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/i586-poky-linux-uclibc/usr/lib/pkgconfig
-CONFIG_SITE=/home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/site-config-i586-poky-linux-uclibc
-INCLUDES=-I /home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/arduino/x86/cores/arduino -I /home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/arduino/x86/variants/galileo_fab_d -I /home/nick/dev/arduino/arduino-1.5.3-Intel.1.0.4/hardware/tools/sysroots/i586-poky-linux-uclibc/usr/include
-TARGET_PREFIX=i586-poky-linux-uclibc-
-CC=i586-poky-linux-uclibc-gcc
-CXX=i586-poky-linux-uclibc-g++
-CPP=i586-poky-linux-uclibc-gcc
-AS=i586-poky-linux-uclibc-as
-LD=i586-poky-linux-uclibc-ld
-GDB=i586-poky-linux-uclibc-gdb
-SIZE=i586-poky-linux-uclibc-size
-STRIP=i586-poky-linux-uclibc-strip
-RANLIB=i586-poky-linux-uclibc-ranlib
-OBJCOPY=i586-poky-linux-uclibc-objcopy
-OBJDUMP=i586-poky-linux-uclibc-objdump
-AR=i586-poky-linux-uclibc-ar
-NM=i586-poky-linux-uclibc-nm
+PKG_CONFIG_SYSROOT_DIR=$(SDK)/hardware/tools/sysroots/i586-poky-linux-uclibc
+PKG_CONFIG_PATH=$(SDK)/hardware/tools/sysroots/i586-poky-linux-uclibc/usr/lib/pkgconfig
+CONFIG_SITE=$(SDK)/hardware/tools/site-config-i586-poky-linux-uclibc
+#BIN=$(SDK)/hardware/tools/
+INCLUDES=-I $(SDK)/hardware/arduino/x86/cores/arduino -I $(SDK)/hardware/arduino/x86/variants/galileo_fab_d -I $(SDK)/hardware/tools/sysroots/i586-poky-linux-uclibc/usr/include
+TARGET=i586-poky-linux-uclibc
+TARGET_PREFIX=$(TARGET)-
+CC=$(TARGET_PREFIX)gcc
+CXX=$(TARGET_PREFIX)g++
+CPP=$(TARGET_PREFIX)gcc
+AS=$(TARGET_PREFIX)as
+LD=$(TARGET_PREFIX)ld
+GDB=$(TARGET_PREFIX)gdb
+SIZE=$(TARGET_PREFIX)size
+STRIP=$(TARGET_PREFIX)strip
+RANLIB=$(TARGET_PREFIX)ranlib
+OBJCOPY=$(TARGET_PREFIX)objcopy
+OBJDUMP=$(TARGET_PREFIX)objdump
+AR=$(TARGET_PREFIX)ar
+NM=$(TARGET_PREFIX)nm
 M4=m4
-CONFIGURE_FLAGS=--target=i586-poky-linux-uclibc --host=i586-poky-linux-uclibc --build=x86_64-linux --with-libtool-sysroot=$(OECORE_TARGET_SYSROOT)
+CONFIGURE_FLAGS=--target=$(TARGET) --host=$(TARGET) --build=x86_64-linux --with-libtool-sysroot=$(OECORE_TARGET_SYSROOT)
 CFLAGS=-m32 -march=i586 -O2 -pipe -g -feliminate-unused-debug-types
 CXXFLAGS=-E -m32 -march=i586 --sysroot=$(OECORE_TARGET_SYSROOT) -O2 -pipe -g -feliminate-unused-debug-types -fpermissive
 LDFLAGS=-Wl,-O1 -Wl,--hash-style=gnu -Wl,--as-needed --sysroot=$(OECORE_TARGET_SYSROOT)
--- makefile
+++ makefile
@@ -15,46 +15,31 @@ ifndef SDK
 $(error SDK is not set. Did you run setup-env.sh?)
 endif
 
-#SDK=../arduino-1.5.3-Intel.1.0.4
-#SDKROOT=$(SDK)/hardware/tools/sysroots/x86_64-pokysdk-linux
-#SDKINC=$(SDK)/hardware/arduino/x86/cores/arduino/
-#VARIANT=$(SDK)/hardware/arduino/x86/variants/galileo_fab_d/
-#SDKLIB=$(SDK)/hardware/arduino/x86/libraries
-
-#TEST=$(shell $(SDK)/hardware/tools/environment-setup-i586-poky-linux-uclibc/environment-setup-i586-poky-linux-uclibc)
-#TEST=$(shell $(SDK)/hardware/arduino/x86/ino_conf.txt)
-
-#tried to source these from ino_conf.txt, but no worky
-#CC_FLAGS=-m32 -march=i586 -g -Os -Wall -ffunction-sections -fdata-sections  -D__ARDUINO_X86__ -DARDUINO=153
-#CXX_FLAGS=-fno-exceptions
-
-#SYSROOT=$(SDK)/hardware/tools/sysroots/i586-poky-linux-uclibc
-#SDKBIN=$(SDKROOT)/usr/bin/i586-poky-linux-uclibc
-#CC=$(SDKBIN)/i586-poky-linux-uclibc-gcc
-#GPP=i586-poky-linux-uclibc-g++
-
 #CFLAGS=-Wall --sysroot=$(SYSROOT) -I$(SDKINC) -I$(VARIANT)
-#LDFLAGS=--sysroot=$(SYSROOT)
 BIN=bin
 SOURCES=$(wildcard src/*.c)
 BUILDDIR=build
 #$(SOURCES:.c=.o)
 OBJS=$(patsubst src/%.c,build/%.o,$(SOURCES))
-TARGET=$(BIN)/carduino
+OBJS+=build/arduino.o
+EXECUTABLE=$(BIN)/carduino
 
 .PHONY: all clean upload
 
-all: $(TARGET)
-	$(STRIP) $(TARGET)
-	@$(SIZE) -A $(TARGET) | grep "Total"
+all: $(EXECUTABLE)
+	$(STRIP) $(EXECUTABLE)
+	@$(SIZE) -A $(EXECUTABLE) | grep "Total"
 
-$(TARGET): $(OBJS)
+$(EXECUTABLE): $(OBJS)
 	$(CC) $(LDFLAGS) $(OBJS) -o $@
 
-build/%.o: src/%.c
+build/%.o: $(SOURCES)
 	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@ $(CFLAGS)
 	
-upload: $(TARGET)
+build/arduino.o:
+	$(CC) $(CFLAGS) $(INCLUDES) -c $(SDK)/hardware/arduino/x86/cores/arduino/wiring_digital.c -o $@ $(CFLAGS)
+	
+upload: $(EXECUTABLE)
 	#scp blah blah
 	
 clean:
